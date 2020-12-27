c842c21c28c8fcba9753e4ca521e0c8de9d2abad
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2020-12-27 12:45:09 +0000

    Cleanup code for 2.10 Creatingour React wrapper

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      personClicks: 0
-    }
-  }
-
-  personClicked = () => {
-    this.setState({ personClicks: this.state.personClicks + 1 });
-  }
-
   render() {
     return (
       <div className="App">
-        <Hello person="Tony" personClicked={this.personClicked} />
-        <Hello person="Mary" personClicked={this.personClicked} />
-        <Hello person="Susan" personClicked={this.personClicked} />
-        <Hello personClicked={this.personClicked} />
-        Person Clicks: {this.state.personClicks}
       </div>
     );
   }
