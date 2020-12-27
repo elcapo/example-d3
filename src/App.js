a029ef51c197656649314a434034f4d92cdc76c2
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2020-12-27 12:51:54 +0000

    Completed until: 2.10 Creating our React wrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChartWrapper from './ChartWrapper';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <ChartWrapper />
       </div>
     );
   }
