64b79f638e789ead5dc063981bb80130006cf831
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2020-12-27 17:42:20 +0000

    Completed until: 3.19 Setting up our project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
 import ChartWrapper from './ChartWrapper';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ChartWrapper />
+        <Navbar bg="light">
+          <Navbar.Brand>Tall People</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <ChartWrapper />
+        </Container>
       </div>
     );
   }
