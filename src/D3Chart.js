a6287965818e643e2253d5bc28e3e1c7d18ad52b
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2020-12-27 18:03:19 +0000

    Completed until: 3.21 Fixing our bar heights

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,22 +1,24 @@
 import * as d3 from "d3";
 
 const url = "http://localhost:3000/data/tallest_men.json";
+const WIDTH = 800;
+const HEIGHT = 500;
 
 class D3Chart {
   constructor(element) {
     const svg = d3.select(element)
       .append("svg")
-      .attr("width", 800)
-      .attr("height", 500);
+      .attr("width", WIDTH)
+      .attr("height", HEIGHT);
 
     d3.json(url).then(data => {
       const y = d3.scaleLinear()
-        .domain([0, 272])
-        .range([0, 500]);
+        .domain([0, d3.max(data, d => d.height)])
+        .range([0, HEIGHT]);
 
       const x = d3.scaleBand()
         .domain(data.map(d => d.name))
-        .range([0, 800])
+        .range([0, WIDTH])
         .padding(0.4);
 
       const rects = svg.selectAll("rect")
@@ -24,8 +26,8 @@ class D3Chart {
 
       rects.enter().append("rect")
         .attr("x", d => x(d.name))
-        .attr("y", 0)
-        .attr("width", 50)
+        .attr("y", d => HEIGHT - y(d.height))
+        .attr("width", x.bandwidth)
         .attr("height", d => y(d.height))
         .attr("fill", "green");
     });
